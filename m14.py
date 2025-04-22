5a953d7355c3c4cf55e2ec3ec54201f6b72f4186
jessicadippolito <jessdipp@gmail.com>
2025-04-22 16:54:12 -0400

    in progress

--- m14.py
+++ m14.py
@@ -1,11 +1,34 @@
-import os
-my_file = open('contest.txt')
-for line in my_file:
-    print(f'{line.split(',')[0]} caught pakuri with the following levels: ', end = '')
-    for i in range (1,len(line.split('-'))-1):
-        print(f'{line.split("-")[i][0]}, ', end = '')
-    print(f'{line.split("-")[(len(line.split('-')))-1]}', end = '')
-print()
-winner = ''
-print(f'Since {winner} caught the highest level pakuri, {winner} is the winner.')
+class Trainer:
+    def __init__(self, name, max_level):
+        self.name = name
+        self.max_level = max_level
 
+
+def read_data():
+    found_pakuris = set()
+    trainers = []
+    with open("contest.txt", "r") as file:
+        for line in file:
+            trainer, *pakuris = line.split(",")
+            max_level = -1
+            for pakuri in pakuris:
+                name, level = pakuri.split("-")
+                level = int(level)
+                found_pakuris.add(name)
+                if level > max_level:
+                    max_level = level
+            trainers.append(Trainer(trainer, max_level))
+    return trainers, found_pakuris
+
+
+def main():
+    trainers, pakuri = read_data()
+    winner = max(trainers, key=lambda t: t.max_level)
+    with open("winner.txt", "w") as file:
+        file.write(winner.name)
+    with open("pakuri.txt", "w") as file:
+        file.write("\n".join(sorted(pakuri)))
+
+
+if __name__ == "__main__":
+    main()
